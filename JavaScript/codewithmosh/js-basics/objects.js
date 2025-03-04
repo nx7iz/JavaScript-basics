d9134864aa3279783c92e36122354128b2a4ea1e
nx7iz <nomannz500@gmail.com>
2025-03-04 21:29:38 +0500

    new projects

--- JavaScript/codewithmosh/js-basics/objects.js
+++ JavaScript/codewithmosh/js-basics/objects.js
@@ -30,28 +30,28 @@
 // const circle1 = createCircle(1);
 // console.log(circle1);
 
-// const circle2 = createCircle(2);
-// console.log(circle2);
+// const another = createCircle(2);
+// console.log(another);
 
 // Constructor Function (Pascal Notation)
 
-function Circle(radius) {
-// this adds the property to the 'new' object
-  this.radius = radius;
-  this.draw = function() {
-    console.log('draw');
-  }
-  this.location = 45;
+// function Circle(radius) {
+// // this adds the property to the 'new' object
+//   this.radius = radius;
+//   this.draw = function() {
+//     console.log('draw');
+//   }
+//   this.location = 45;
 
-}
+// }
 
 
-const circle = Circle.call({}, 1)
+// const circle = new Circle(1);
 /* 1. new creates a empty object (x = {})
    2. sets 'this' to point to new object
    3. returns this object
 */
-console.log(circle);
+// console.log(circle);
 
 
 // Built-in Constructor function for String
@@ -71,9 +71,77 @@ console.log(circle);
 // console.log(circle);
 
 
+// let x = { value: 10};
+// let y = x;
+
+// x.value = 20;
+
+// let obj = { value: 10 };
+// function increase(obj) {
+//   obj.value++;
+// }
+
+// increase(obj);
+// console.log(obj);
+
+ 
+// const circle = {
+//   radius: 1,
+//   draw() {
+//     console.log('draw');
+//   }
+// };
+
+// for (let key in circle) 
+//   console.log(key, circle[key]);
+
+// // Object.keys gives us string array
+// for (let key of Object.keys(circle)) 
+//   console.log(key);
+
+// // Object.entries gives us array
+// for (let entry of Object.entries(circle)) 
+//   console.log(entry);
+
+// // 'in' to check if a property exists in object
+// if ('radius' in circle) console.log('yes');
+// if ('location' in circle) 
+//   console.log('yes');
+// else 
+//   console.log('no');
 
 
+// function Object() {}
+// const x = { value: 1 };
+// const x = new Object();
 
 
+// const circle = {
+//   radius: 1,
+//   draw() {
+//     console.log('draw');
+//   }
+// };
 
+
+// // let another = {};
+// // for (let key in circle) {
+// //   another[key] = circle[key];
+// //   console.log(another);
+// // }
+
+
+// // Object.assign copies the properties and methods from one or more source objects into targeted object to clone or combine multiple objects into single object
+
+// // let another = Object.assign({}, circle);
+
+// // 2nd way
+// // let another = Object.assign({
+// //   color: 'yellow'
+// // }, circle);
+
+// // ... --> spread (takes all the properties and methods of targeted object and put it {...} here, where we call spread operator)
+// const another = { ...circle };
+
+// console.log(another);
 
