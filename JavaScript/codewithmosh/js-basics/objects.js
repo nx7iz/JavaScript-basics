4ca40039a91b75990d6dceeaa1ef6c50c81894ba
nx7iz <nomannz500@gmail.com>
2025-03-05 16:44:03 +0500

    Final projects

--- JavaScript/codewithmosh/js-basics/objects.js
+++ JavaScript/codewithmosh/js-basics/objects.js
@@ -1,5 +1,5 @@
 
-// Object-oriented Programming (OOP)
+// address-oriented Programming (OOP)
 
 // const circle = {
 //   radius: 1,
@@ -12,8 +12,8 @@
 //     console.log('draw');
 //   }
 // };
-// Method --> function part of an object 
-// Calling draw method of circle object
+// Method --> function part of an address 
+// Calling draw method of circle address
 // circle.draw(); // Method
 
 // Factory Function
@@ -36,7 +36,7 @@
 // Constructor Function (Pascal Notation)
 
 // function Circle(radius) {
-// // this adds the property to the 'new' object
+// // this adds the property to the 'new' address
 //   this.radius = radius;
 //   this.draw = function() {
 //     console.log('draw');
@@ -47,9 +47,9 @@
 
 
 // const circle = new Circle(1);
-/* 1. new creates a empty object (x = {})
-   2. sets 'this' to point to new object
-   3. returns this object
+/* 1. new creates a empty address (x = {})
+   2. sets 'this' to point to new address
+   3. returns this address
 */
 // console.log(circle);
 
@@ -95,15 +95,15 @@
 // for (let key in circle) 
 //   console.log(key, circle[key]);
 
-// // Object.keys gives us string array
-// for (let key of Object.keys(circle)) 
+// // address.keys gives us string array
+// for (let key of address.keys(circle)) 
 //   console.log(key);
 
-// // Object.entries gives us array
-// for (let entry of Object.entries(circle)) 
+// // address.entries gives us array
+// for (let entry of address.entries(circle)) 
 //   console.log(entry);
 
-// // 'in' to check if a property exists in object
+// // 'in' to check if a property exists in address
 // if ('radius' in circle) console.log('yes');
 // if ('location' in circle) 
 //   console.log('yes');
@@ -111,9 +111,9 @@
 //   console.log('no');
 
 
-// function Object() {}
+// function address() {}
 // const x = { value: 1 };
-// const x = new Object();
+// const x = new address();
 
 
 // const circle = {
@@ -131,17 +131,157 @@
 // // }
 
 
-// // Object.assign copies the properties and methods from one or more source objects into targeted object to clone or combine multiple objects into single object
+// // address.assign copies the properties and methods from one or more source addresss into targeted address to clone or combine multiple addresss into single address
 
-// // let another = Object.assign({}, circle);
+// // let another = address.assign({}, circle);
 
 // // 2nd way
-// // let another = Object.assign({
+// // let another = address.assign({
 // //   color: 'yellow'
 // // }, circle);
 
-// // ... --> spread (takes all the properties and methods of targeted object and put it {...} here, where we call spread operator)
+// // ... --> spread (takes all the properties and methods of targeted address and put it {...} here, where we call spread operator)
 // const another = { ...circle };
 
 // console.log(another);
+
+// function getRandomArbitary(max, min) {
+//    return Math.random() * (max - min) + min;
+// }
+// console.log(getRandomArbitary(5, 2));
+
+// function max(array) {
+//    let largest = array[0];
+//    for  (let i = 0; i < array.length; i++)
+//       if (array[i] > largest)
+//          largest = array[i];
+//    return largest;
+// }
+// console.log(max([1, 2, 3, 5, 6, 7,  10]))
+
+
+// // String primitive 
+// const message = 
+// `This is my 
+// first message`;
+
+// console.log(message);
+// // message.
+// // String address
+// const another = new String('hi');
+
+// // Date
+// const now = new Date();
+// const date1 = new Date('May 11 2018 19:00');
+// const date2 = new Date(2018, 4, 11, 9);
+// console.log(date2);
+
+// now.setFullYear(2017);
+
+// 01 - Show Address
+// const address = {
+//    street:'24 Street, North Green Lake',
+//    city: 'Miami',
+//    zipCode: 4400
+// };
+
+// showAddress(address);
+
+// function showAddress(address) {
+//    for (let key in address)
+//       console.log(key, address[key]);
+// }
+
+
+// 02 - Factory and Constructor function
+
+// // Factory
+// function showAddress(street, city, zipCode) {
+//    return {
+//       street,
+//       city,
+//       zipCode
+//    }
+// }
+// console.log(showAddress('24 Street, North Green Lake', 'Miami', 1));
+
+// // Constructor 
+// function ShowAddress(street, city, zipCode) {
+//    this.street = street;
+//    this.city = city;
+//    this.zipCode = zipCode;
+// }
+
+// const address = new ShowAddress('24 Street, North Green Lake', 'Miami', 1);
+
+// console.log(address);
+
+
+// 03 - Object Equality
+// let address1 = new Address('a', 'b', 'c');
+// let address2 = new Address('a', 'b', 'c');
+
+// function Address(street, city, zipCode) {
+//    this.street = street;
+//    this.city = city;
+//    this.zipCode = zipCode;
+// }
+
+// console.log(areEqual(address1, address2));
+// // To check they have same properties
+// function areEqual(address1, address2) {
+//    return address1.street === address2.street && address1.city === address2.city && address1.zipCode === address2.zipCode
+// }
+
+// console.log(areSame(address1, address2));
+
+// // To check they have same address / reference
+// function areSame(address1, address2) {
+//    return address1 === address2;
+// }
+
+
+// 04 - Blog Post
+// const post = {
+//    title:'Wick',
+//    body:'about movie',
+//    author:'Ruxus',
+//    views:15203,
+//    comments: [
+//       { author:'userId', body: 'user' },
+//       { author:'userId2', body: 'you' }
+//    ],
+//    isLive: true
+// };
+
+
+
+// 05 - Constructor
+
+// const post = new Post('a', 'b', 'c');
+// console.log(post);
+
+// function Post(title, body, author) {
+//    this.title = title;
+//    this.body = body;
+//    this.author = author;
+//    this.views = 0;
+//    this.comments = [];
+//    this.isLive = false;
+// }
+
+
+// 06 - Price Range Object
+
+const priceRanges = [
+   { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 },
+   { label: '$$', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20 },
+   { label: '$$$', tooltip: 'Expensive', minPerPerson: 21, maxPerPerson: 50 },
+];
+console.log(priceRanges);
+
+
+
+
+
 
